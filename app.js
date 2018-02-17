cd102c51e523b5cbf0c22bf2748ca80a33ab03cb
Greg Bateman <gerbot150@gmail.com>
2018-02-17 16:54:28 -0500

    Updates to config

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 /*
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
- *
  */
-
-/* jshint node: true, devel: true */
-'use strict';
 
 const
   bodyParser = require('body-parser'),
@@ -20,7 +13,7 @@ const
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
@@ -82,6 +75,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
+  console.log(data);
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
